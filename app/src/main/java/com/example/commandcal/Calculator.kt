588ae49dea4b50be39333d2a35fd5d92ba47079f
Sooyoung Hwang <agriades23@gmail.com>
2024-03-08 20:28:50 +0900

    Lv 2 완료: mod(%) 기능 추가, input 받아 결과 내놓기

--- app/src/main/java/com/example/commandcal/Calculator.kt
+++ app/src/main/java/com/example/commandcal/Calculator.kt
@@ -4,7 +4,7 @@ class Calculator {
     fun addIs(num1: Int, num2: Int) {
         println("${num1} 더하기 ${num2}은(는) ${num1+num2} 입니다.")
     }
-    fun subtractIs(num1: Int, num2: Int) {
+        fun subtractIs(num1: Int, num2: Int) {
         println("${num1} 빼기 ${num2}은(는) ${num1-num2} 입니다.")
     }
     fun multiplyIs(num1: Int, num2: Int) {
@@ -14,4 +14,7 @@ class Calculator {
         println("${num1} 을 ${num2}으로 나눈 몫은 ${num1/num2} 입니다.")
     }
 
+    fun modIs(num1: Int, num2: Int) {
+        println("${num1} 을 ${num2}으로 나눈 나머지는 ${num1%num2} 입니다.")
+    }
 }
--- app/src/main/java/com/example/commandcal/Main.kt
+++ app/src/main/java/com/example/commandcal/Main.kt
@@ -1,11 +1,23 @@
 package com.example.commandcal
 
 fun main(){
+    println("첫 번째 정수 입력: ")
+    val num1 = readLine()!!.toInt()
+    println("[1] 덧셈, [2]뺄셈, [3]곱셈, [4]나눗셈, [5]나머지, [-1]종료")
+    val sign = readLine()!!.toInt()
+    println("두 번째 정수 입력: ")
+    val num2 = readLine()!!.toInt()
+
     val c = Calculator()
-    println("hello world!")
-    c.addIs(3,8)
-    c.subtractIs(3,8)
-    c.multiplyIs(3,8)
-    c.divideIs(20243,8)
+
+    when (sign){
+        1 -> c.addIs(num1,num2)
+        2 -> c.subtractIs(num1,num2)
+        3 -> c.multiplyIs(num1,num2)
+        4 -> c.divideIs(num1,num2)
+        5 -> c.modIs(num1,num2)
+    }
+
+    println("끝!")
 
 }
