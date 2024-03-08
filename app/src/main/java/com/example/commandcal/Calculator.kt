83dc34fd1c91b45b59e72ea391dc6c3433bd172d
Sooyoung Hwang <agriades23@gmail.com>
2024-03-08 20:18:34 +0900

    콘솔 내에서 println으로 모두 확인할 수 있게 하기! 이게 진짜 lv 1

---
+++ app/src/main/java/com/example/commandcal/Calculator.kt
@@ -0,0 +1,17 @@
+package com.example.commandcal
+
+class Calculator {
+    fun addIs(num1: Int, num2: Int) {
+        println("${num1} 더하기 ${num2}은(는) ${num1+num2} 입니다.")
+    }
+    fun subtractIs(num1: Int, num2: Int) {
+        println("${num1} 빼기 ${num2}은(는) ${num1-num2} 입니다.")
+    }
+    fun multiplyIs(num1: Int, num2: Int) {
+        println("${num1} 곱하기 ${num2}은(는) ${num1*num2} 입니다.")
+    }
+    fun divideIs(num1: Int, num2: Int) {
+        println("${num1} 을 ${num2}으로 나눈 몫은 ${num1/num2} 입니다.")
+    }
+
+}
--- app/src/main/java/com/example/commandcal/Main.kt
+++ app/src/main/java/com/example/commandcal/Main.kt
@@ -1,5 +1,11 @@
 package com.example.commandcal
 
 fun main(){
+    val c = Calculator()
     println("hello world!")
+    c.addIs(3,8)
+    c.subtractIs(3,8)
+    c.multiplyIs(3,8)
+    c.divideIs(20243,8)
+
 }
